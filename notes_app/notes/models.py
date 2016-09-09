5d4f50d3eb76ea22565192f0d6d85944ea73af89
Denis Glebovskiy <Denis_Glebovskiy@epam.com>
2016-09-09 11:06:29 +0300

    Model field and migration for sharing notes with other users

--- notes_app/notes/models.py
+++ notes_app/notes/models.py
@@ -5,11 +5,19 @@ from django.contrib.auth.models import User
 
 
 class Note(models.Model):
-    title = models.CharField("Title", max_length=255)
-    content = models.TextField("Content")
-    owner = models.ForeignKey(User, models.CASCADE, verbose_name="Owner")
-    created_on = models.DateTimeField("Created on", auto_now_add=True)
-    updated_on = models.DateTimeField("Updated on", auto_now=True)
+    title = models.CharField('Title', max_length=255)
+    content = models.TextField('Content')
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Owner')
+    shared_with = models.ManyToManyField(
+        User,
+        blank=True,
+        related_name='shared_notes'
+    )
+    created_on = models.DateTimeField('Created on', auto_now_add=True)
+    updated_on = models.DateTimeField('Updated on', auto_now=True)
 
     def __unicode__(self):
         return self.title
