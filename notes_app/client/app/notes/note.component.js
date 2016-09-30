497bc90da96d924114cf4248ade4973eaacf3338
Denis Glebovskiy <Denis_Glebovskiy@epam.com>
2016-09-30 15:06:12 +0300

    Form for editing notes (w/o design)

--- notes_app/client/app/notes/note.component.js
+++ notes_app/client/app/notes/note.component.js
@@ -3,8 +3,29 @@
 
   function NoteCtrl(notesService) {
     var ctrl = this;
+    ctrl.isEditing = false;
+    ctrl.notecopy = {};
     ctrl.deleteNote = function () {
       notesService.delete(this.note.id);
+    };
+    ctrl.toggleEditing = function() {
+      ctrl.isEditing = !ctrl.isEditing;
+      ctrl.notecopy['title'] = ctrl.note.title;
+      ctrl.notecopy['content'] = ctrl.note.content;
+    };
+    ctrl.updateNote = function() {
+      notesService.update(ctrl.note.id, ctrl.notecopy).then(function(status) {
+        if (status) {
+          ctrl.note.title = ctrl.notecopy.title;
+          ctrl.note.content = ctrl.notecopy.content;
+          ctrl.isEditing = false;
+        } 
+      })
+    };
+    ctrl.resetNote = function() {
+      delete ctrl.notecopy.title;
+      delete ctrl.notecopy.content;
+      ctrl.isEditing = false;
     };
   }
 
