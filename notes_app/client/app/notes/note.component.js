f048b19d8dad7520b6ceb504a3ae5e131975e127
Denis Glebovskiy <Denis_Glebovskiy@epam.com>
2016-10-05 09:43:08 +0300

    Note updates now send user resource paths to server correctly

--- notes_app/client/app/notes/note.component.js
+++ notes_app/client/app/notes/note.component.js
@@ -11,26 +11,30 @@
     };
     ctrl.toggleEditing = function() {
       ctrl.isEditing = !ctrl.isEditing;
-      ctrl.notecopy['title'] = ctrl.note.title;
-      ctrl.notecopy['content'] = ctrl.note.content;
-      ctrl.notecopy['shared_with'] = ctrl.note.shared_with;
+      ctrl.notecopy.title = ctrl.note.title;
+      ctrl.notecopy.content = ctrl.note.content;
+      ctrl.notecopy.selectedUsers = ctrl.note.shared_with;
     };
     ctrl.updateNote = function() {
+      ctrl.notecopy.shared_with = ctrl.notecopy.selectedUsers.map(function(user) {
+        return user.resource_uri;
+      });
       notesService.update(ctrl.note.id, ctrl.notecopy).then(function(status) {
         if (status) {
           ctrl.note.title = ctrl.notecopy.title;
           ctrl.note.content = ctrl.notecopy.content;
-          ctrl.note.shared_with = ctrl.notecopy.shared_with;
+          ctrl.note.shared_with = ctrl.notecopy.selectedUsers;
           ctrl.isEditing = false;
         }
       })
     };
     ctrl.updateSelected = function(selected) {
-      ctrl.notecopy.shared_with = selected;
+      ctrl.notecopy.selectedUsers = selected;
     }
     ctrl.resetNote = function() {
       delete ctrl.notecopy.title;
       delete ctrl.notecopy.content;
+      delete ctrl.notecopy.selectedUsers;
       delete ctrl.notecopy.shared_with;
       ctrl.isEditing = false;
     };
