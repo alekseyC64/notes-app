7283308b31f5bfedd0a38a00fb61283d0f537f46
dglebovskiy <Denis_Glebovskiy@epam.com>
2016-10-19 11:24:11 +0300

    Gulp build script updates (#39)
    * Gulp build script updates:
    - index.html refers to files in node_modules again
    - Gulp-useref concats css and script files
    - Gulp builds the project to the dist folder
    - Django now serves static files from the dist folder
    
    * CSS and JS minification
    - css is minified
    - external libraries' js is minified
    
    * Added missing gulp plugin to dependencies
    
    * Added watchers on templates and script files

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,36 @@
 const gulp = require('gulp'),
-    karma = require('karma')
+      concat = require('gulp-concat'),
+      gulpif = require('gulp-if'),
+      minifyCss = require('gulp-clean-css'),
+      notify = require('gulp-notify'),
+      uglify = require('gulp-uglify'),
+      useref = require('gulp-useref'),
+      karma = require('karma')
 
-const scripts = [
-  'node_modules/angular/angular.js',
-  'node_modules/angular-sanitize/angular-sanitize.js',
-  'node_modules/angular-ui-router/release/angular-ui-router.js',
-  'node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
-  'node_modules/ui-select/dist/select.js',
-  'node_modules/angular-ui-grid/ui-grid.js'
-]
-
-const styles = [
-  'node_modules/bootstrap-css-only/css/bootstrap.css',
-  'node_modules/bootstrap-css-only/css/bootstrap-theme.css',
-  'node_modules/ui-select/dist/select.css',
-  'node_modules/angular-ui-grid/ui-grid.css'
-]
-
-gulp.task('scripts', function() {
-  gulp.src(scripts).pipe(gulp.dest('notes_app/client/scripts/'))
+gulp.task('copytemplates', function() {
+  return gulp
+    .src('./notes_app/client/**/*.tpl.html')
+    .pipe(gulp.dest('./notes_app/dist'))
+    .pipe(notify({
+      'message': 'Templates copied to dist',
+      'onLast': true
+    }))
 })
 
-gulp.task('styles', function() {
-  gulp.src(styles).pipe(gulp.dest('notes_app/client/styles/'))
+gulp.task('build', function() {
+  return gulp
+    .src('./notes_app/client/index.html')
+    .pipe(useref())
+    .pipe(gulpif('libs.js', uglify()))
+    .pipe(gulpif('*.css', minifyCss()))
+    .pipe(gulp.dest('./notes_app/dist'))
+    .pipe(notify({
+      'message': 'Build complete',
+      'onLast': true
+    }))
 })
 
-gulp.task('default', ['scripts', 'styles'])
+gulp.task('default', ['copytemplates', 'build'])
 
 gulp.task('test', ['default'], function(done) {
   const s = new karma.Server({
@@ -33,4 +38,9 @@ gulp.task('test', ['default'], function(done) {
     'singleRun': true
   }, done)
   s.start()
+})
+
+gulp.task('watch', ['default'], function() {
+  gulp.watch('./notes_app/client/**/*.tpl.html', ['copytemplates'])
+  gulp.watch('./notes_app/client/**/*.js', ['build'])
 })
--- notes_app/notes_app/settings.py
+++ notes_app/notes_app/settings.py
@@ -106,7 +106,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'client')
+    os.path.join(BASE_DIR, 'dist')
 ]
 
 
