1bc74957189147d9012f21f62cdaa739a9fe3289
Denis Glebovskiy <Denis_Glebovskiy@epam.com>
2016-10-21 15:51:07 +0300

    Packing templates into angular's templatecache

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,31 @@
 const gulp = require('gulp'),
       concat = require('gulp-concat'),
       gulpif = require('gulp-if'),
+      inject = require('gulp-inject'),
       minifyCss = require('gulp-clean-css'),
       notify = require('gulp-notify'),
+      templateCache = require('gulp-angular-templatecache'),
       uglify = require('gulp-uglify'),
       useref = require('gulp-useref'),
       karma = require('karma')
 
-gulp.task('copytemplates', function() {
+gulp.task('buildtemplates', function() {
   return gulp
     .src('./notes_app/client/**/*.tpl.html')
+    .pipe(templateCache())
     .pipe(gulp.dest('./notes_app/dist'))
     .pipe(notify({
-      'message': 'Templates copied to dist',
-      'onLast': true
+      'message': 'Templates packed into template cache'
     }))
 })
 
-gulp.task('build', function() {
+gulp.task('build', ['buildtemplates'], function() {
   return gulp
     .src('./notes_app/client/index.html')
+    .pipe(inject(gulp.src('./notes_app/dist/templates.js'), {
+      name: 'templatecache',
+      relative: true
+    }))
     .pipe(useref())
     .pipe(gulpif('libs.js', uglify()))
     .pipe(gulpif('*.css', minifyCss()))
@@ -30,7 +36,7 @@ gulp.task('build', function() {
     }))
 })
 
-gulp.task('default', ['copytemplates', 'build'])
+gulp.task('default', ['build'])
 
 gulp.task('test', ['default'], function(done) {
   const s = new karma.Server({
